0dfb5fe6d565d35b8fd16ac2e7dfa8087b91bfe4
OS <duonggiaothien@gmail.com>
2023-12-12 09:50:42 +0700

    declare module and add attribute customer/product

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 1 All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Tigren_PWA', __DIR__);
+
